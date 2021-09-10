9ba1e0587fa5054b0e8716f415f3a53bbc9dfee8
Isabell Lee <izzy.isabell@gmail.com>
2021-09-10 13:43:43 -0700

    Created a ProductOverview component and folder, created Components folder

---
+++ client/src/Components/ProductOverview/ProductOverview.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ProductOverview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='po-main'>
+        Product Overview Component Stand-in
+      </div>
+    );
+  }
+}
+
+export default ProductOverview;
