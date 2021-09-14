505cec44724d87915e58dfbff32f95301789487a
Isabell Lee <izzy.isabell@gmail.com>
2021-09-13 17:36:35 -0700

    Wrote inital test for PO that renders App component, added .babelrc, refactored index.jsx to import App from app.jsx

---
+++ client/src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ProductOverview from './Components/ProductOverview/ProductOverview.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello from React! This is a test for merge conflict from Iris.
+        <ProductOverview />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ tests/po.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from '../client/src/app.jsx';
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+    screen.debug();
+  });
+});
