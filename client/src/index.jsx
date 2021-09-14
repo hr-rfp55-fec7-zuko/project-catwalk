47a8ded2fa774f8d71e98782fa2a347b8de1a455
Izzy Lee <86844794+isabell-lee@users.noreply.github.com>
2021-09-14 12:41:02 -0700

    Update index.jsx
    add back in refactor to app.jsx separate App component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ProductOverview from './Components/ProductOverview/ProductOverview.jsx';
-import RelatedProductsView from './Components/RelatedProducts/RelatedProductsView.jsx';
-import YourOutfitList from './Components/YourOutfitList/YourOutfitList.jsx';
-
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello from React! This is a test for merge conflict from Iris.
-        <ProductOverview />
-        <RelatedProductsView />
-        <YourOutfitList />
-      </div>
-    );
-  }
-}
+import App from './app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
