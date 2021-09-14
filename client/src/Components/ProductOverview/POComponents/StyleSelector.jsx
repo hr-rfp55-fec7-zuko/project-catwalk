30f13a86735672b541d9f1eed32b61de3f8c0e92
Isabell Lee <izzy.isabell@gmail.com>
2021-09-14 15:34:13 -0700

    StyleSelector displays all style imgs

--- client/src/Components/ProductOverview/POComponents/StyleSelector.jsx
+++ client/src/Components/ProductOverview/POComponents/StyleSelector.jsx
@@ -12,20 +12,22 @@ class StyleSelector extends React.Component {
   render() {
     return (
       <div id='po-style-select'>
-        Style Selected: {this.state.styleName}
+        <h4>Style Selected: {this.state.styleName}</h4>
+        {this.state.styles.map((style) => <img className='style-img' src={style.photos[0].thumbnail_url} />)}
       </div>
     );
   }
 
-  componentDidMount() {
-    console.log('running componentDidMount when', this.state.styles );
-    this.setState({styles: this.props.styles});
-    console.log(this.props.styles.length);
-    for (var i = 0; i < this.props.styles.length; i++) {
-      console.log('going into for loop');
-      if (styles[i]['default?']) {
-        this.setState({styleName: styles[i].name});
-        this.props.setStyleSelection(styles[i]);
+  componentDidUpdate(prevProps) {
+    if (this.props.styles !== prevProps.styles) {
+      this.setState({ styles: this.props.styles });
+      for (var i = 0; i < this.props.styles.length; i++) {
+        console.log('going into for loop');
+        if (this.props.styles[i]['default?']) {
+          this.setState({ styleName: this.props.styles[i].name });
+          this.props.setStyleSelection(this.props.styles[i]);
+          break;
+        }
       }
     }
   }
