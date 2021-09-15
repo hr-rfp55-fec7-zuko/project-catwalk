034fb143dd903690f80109fd039cd133af02da32
Iris Huang <yilu.huang@gmail.com>
2021-09-14 17:17:23 -0700

    Installed Moment.js for date format feature

--- client/src/Components/QuestionAnswer/AnswerEntryList.jsx
+++ client/src/Components/QuestionAnswer/AnswerEntryList.jsx
@@ -1,32 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 var AnswerEntryList = (props) => {
   const [answerList, setAnswerList] = useState([]);
   const dateFormat = { year: 'numeric', month: 'long', day: 'numeric' };
-
   useEffect(()=> {
     let mounted = true;
-    GetAnswerList(props)
+    GetAnswerList(props.questionId)
       .then(items => {
         if (mounted) {
           setAnswerList(items.data);
         }
       });
     return () => mounted = false;
-  });
+  }, [props.questionId]);
   console.log('🥰🥰', answerList);
 
   return (
     <div>
       {answerList.map(item => {
+        console.log(moment(item.date).format('LL'));
         return (
           <div>
             <div className="qa-answers-main">
               {item.body}
             </div>
             <div className="qa-answers-side">
-              {'by' + item.answerer_name + ',' + item.date}
+              {'by' + item.answerer_name + ', ' + moment(item.date).format('LL')}
             </div>
           </div>
         );
@@ -36,9 +37,9 @@ var AnswerEntryList = (props) => {
   );
 };
 
-var GetAnswerList = (props) => {
+var GetAnswerList = (num) => {
   return (
-    axios.get('/qa/questions/:question_id/answers', {params: {qId: props.questionId}})
+    axios.get('/qa/questions/:question_id/answers', {params: {qId: num}})
       .then(response => {
         return response;
       })
