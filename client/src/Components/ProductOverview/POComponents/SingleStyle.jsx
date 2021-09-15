d371b9d32dcb25bb8cdfd95390494d988e1f10e7
Isabell Lee <izzy.isabell@gmail.com>
2021-09-15 00:51:43 -0700

    Still figuring out CSS for style display

--- client/src/Components/ProductOverview/POComponents/SingleStyle.jsx
+++ client/src/Components/ProductOverview/POComponents/SingleStyle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 var SingleStyle = ({ selected, onClick, style }) => {
   return (
-    <div>
+    <div className='style-element'>
       <img className={selected ? 'style-img selected' : 'style-img'} src={style.photos[0].thumbnail_url} onClick={onClick}
       />
     </div>
