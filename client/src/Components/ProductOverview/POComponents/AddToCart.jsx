49e82b2f72de1ab5284d45042f3842f729ea5b96
Isabell Lee <izzy.isabell@gmail.com>
2021-09-15 13:37:04 -0700

    Quantity menu renders based on selected size stock amt, and updates state with select count. Add to cart logs desired item

--- client/src/Components/ProductOverview/POComponents/AddToCart.jsx
+++ client/src/Components/ProductOverview/POComponents/AddToCart.jsx
@@ -8,13 +8,28 @@ class AddToCart extends React.Component {
     this.state = {
     };
     this.setSKU = this.setSKU.bind(this);
+    this.setQuantity = this.setQuantity.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
   }
 
   setSKU(sku) {
     this.setState({
       selectedSKU: sku,
-      quantity: this.props.style.skus[sku].quantity
+      totalQuantity: this.props.style.skus[sku].quantity
     });
+  }
+
+  setQuantity(quantity) {
+    this.setState({quantity});
+  }
+
+  handleAddToCart(e) {
+    e.preventDefault();
+    var cartItem = {
+      'sku_id': this.state.selectedSKU,
+      count: this.state.quantity
+    };
+    console.log('you want to add to cart:', cartItem);
   }
 
   render() {
@@ -22,9 +37,9 @@ class AddToCart extends React.Component {
       <div className='po-add-cart'>
         <div className='select-menus'>
           <SizeSelector skus={this.props.style.skus} setSKU={this.setSKU} />
-          <QuantitySelector />
+          <QuantitySelector totalQuantity={this.state.totalQuantity} setQuantity={this.setQuantity} />
         </div>
-        <button>Add to Cart</button>
+        <button onClick={this.handleAddToCart} >Add to Cart</button>
       </div>
     );
   }
--- client/src/Components/ProductOverview/POComponents/QuantitySelector.jsx
+++ client/src/Components/ProductOverview/POComponents/QuantitySelector.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 
-var QuantitySelector = (props) => {
+var QuantitySelector = ({totalQuantity = 0, setQuantity}) => {
+  if (totalQuantity > 0) {
+    console.log(totalQuantity);
+    var maxLimit = (totalQuantity <= 15 ) ? totalQuantity : 15;
+    var quantOptions = [];
+    for (var i = 1; i <= maxLimit; i++) {
+      quantOptions.push(i);
+    }
+    return (
+      <div>
+        <select id='po-select-qty' onChange={(e) => {
+          e.preventDefault();
+          setQuantity(parseInt(event.target.value));
+        }} >
+          {quantOptions.map((opt) => (<option value={opt} >{opt}</option>))}
+        </select>
+      </div>
+    );
+  }
   return (
     <div>
       <select id='po-select-qty' >
