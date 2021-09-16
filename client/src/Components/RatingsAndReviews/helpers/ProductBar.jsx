4874f569aebf7afb417cac9d04187002e4074118
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-16 10:54:18 -0700

    CSS Updates in attempt to make buffer div render  - WIP

--- client/src/Components/RatingsAndReviews/helpers/ProductBar.jsx
+++ client/src/Components/RatingsAndReviews/helpers/ProductBar.jsx
@@ -21,15 +21,15 @@ const ProductBar = function ProductBar(props) {
   let fill3 = false;
 
   if (overallPercentage < 34) {
-    var innerPercentage = overallPercentage / 30;
+    var innerPercentage = `${(overallPercentage / 30) * 100}%`;
     fill1 = true;
 
   } else if (overallPercentage < 67) {
-    var innerPercentage = (overallPercentage - 33) / 30;
+    var innerPercentage = `${((overallPercentage - 33) / 30) * 100}%`;
     fill2 = true;
 
   } else if (overallPercentage < 101) {
-    var innerPercentage = (overallPercentage - 66) / 30;
+    var innerPercentage = `${((overallPercentage - 66) / 30) * 100}%`;
     fill3 = true;
   }
 
