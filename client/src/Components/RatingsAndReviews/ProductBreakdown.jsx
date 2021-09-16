06918354fb8744c7e5fc87992c4e8cdefb576edc
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-16 00:15:00 -0700

    Helper function renders div with hard coded data.

--- client/src/Components/RatingsAndReviews/ProductBreakdown.jsx
+++ client/src/Components/RatingsAndReviews/ProductBreakdown.jsx
@@ -1,4 +1,46 @@
 import React from 'react';
+import ProductBar from './helpers/ProductBar.jsx';
+
+class ProductBreakdown extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    // let characteristics = this.props.characteristics;
+
+    // let characteristicBars = [];
+
+    // for (var characteristic in characteristics) {
+    //   characteristicBars.push(
+    //     <div className='characteristic-breakdown' data-testid="characteristic-breakdown">
+    //       {characteristic}
+    //       <div className='characteristic-bar-outer'>Bar Here: {characteristics[characteristic].value}</div>
+    //       <span>label low</span> <span>label high</span>
+    //     </div>
+    //   );
+    // }
+
+    let characteristic = 'Size';
+    let score = 4;
+
+
+    //NOTE: NEED 1 BAR FOR EACH - MAP
+    return (
+      <div className="product-breakdown">
+        <h3>Product Breakdown</h3>
+        <ProductBar key={characteristic} characteristic={characteristic} score={4}/>
+      </div>
+    );
+  }
+}
+
+export default ProductBreakdown;
+
+
+
+/*
 
 class ProductBreakdown extends React.Component {
   constructor(props) {
@@ -31,3 +73,5 @@ class ProductBreakdown extends React.Component {
 }
 
 export default ProductBreakdown;
+
+*/
---
+++ client/src/Components/RatingsAndReviews/helpers/ProductBar.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const ratingMeanings = {
+  'Size': ['A size too small', 'A size too wide'],
+  'Width': ['Too narrow', 'Too wide'],
+  'Comfort': ['Uncomfortable', 'Perfect'],
+  'Quality': ['Poor', 'Perfect'],
+  'Length': ['Runs Short', 'Runs long'],
+  'Fit': ['Runs tight', 'Runs long']
+};
+
+//props should include
+const ProductBar = function ProductBar(props) {
+  let characteristic = props.characteristic;
+  let score = props.score;
+
+  console.log(characteristic, score);
+
+  //Math
+  let overallPercentage = (score / 5 ) * 100;
+  let fill1 = false;
+  let fill2 = false;
+  let fill3 = false;
+
+  if (overallPercentage < 34) {
+    var innerPercentage = overallPercentage / 30;
+    fill1 = true;
+
+  } else if (overallPercentage < 67) {
+    var innerPercentage = (overallPercentage - 33) / 30;
+    fill2 = true;
+
+  } else if (overallPercentage < 101) {
+    var innerPercentage = (overallPercentage - 66) / 30;
+    fill3 = true;
+  }
+
+  //Styles
+  var style = {
+    content: 'I\'m alive!'
+  };
+
+  //Variables for conditional rendering
+  var nonTargetBar = <div className='product-bar-inner-fill' />;
+  var targetBar = <div className='product-bar-inner-fill-target' style={style} />;
+
+  return (
+    <>
+      <div className="product-bar-container">
+        <div className='product-bar-outer'>
+          {fill1 === true ? targetBar : nonTargetBar}
+          {ratingMeanings[characteristic][0]}
+          {fill2 === true ? targetBar : nonTargetBar}
+          {fill3 === true ? targetBar : nonTargetBar}
+          {ratingMeanings[characteristic][1]}
+        </div>
+      </div>
+      <br/>
+    </>
+  );
+};
+
+
+export default ProductBar;
+
+// style={{width: `${percentage}%`}}
+
