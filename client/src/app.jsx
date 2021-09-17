3ada03595ebd6325a5cdf12f8533664df1dfd142
Iris Huang <yilu.huang@gmail.com>
2021-09-17 13:55:14 -0700

    Revert back product id to 40344

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      'product_id': '40351',
+      'product_id': '40344',
       'product_name': 'Camo Onesie',
       avgRating: null,
       totalReviews: null
