6ece8dfe29532e50b230f3594dc48036ae3689cf
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-18 12:37:39 -0700

    removes background color and border from star buttons

--- client/src/Components/RatingsAndReviews/helpers/StarPicker.jsx
+++ client/src/Components/RatingsAndReviews/helpers/StarPicker.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar  } from '@fortawesome/free-solid-svg-icons';
-// import { faStar } from 'react-icons/fa'
-
-//Should receive onClick function that tweaks state in the list instance
 
 class StarPicker extends React.Component{
   constructor(props) {
@@ -14,28 +10,23 @@ class StarPicker extends React.Component{
   }
 
   handleStarSelect(event) {
-    // console.log(event.target.parentElement.parentElement)
     let button = event.target.parentElement.parentElement
-    let id = button.id;
     let name = button.name;
-    console.log(button, id, name)
+    let id = button.id;
+
     this.props.handleStarSelect(name, id)
   }
 
   render() {
-
     return (
       <div className="star-picker">
         {[...Array(5)].map((star, i) => {
             const ratingValue = i + 1;
             return (
               <>
-                {/* <input type="radio" name="rating" onChange={this.props.handleRadioFormChange} id={ratingValue} name="rating"/>
-                <label> */}
                 <button type="button" onClick={this.handleStarSelect} id={ratingValue} name="rating">
                   <FontAwesomeIcon onClick={this.handleRadioFormChange} key={ratingValue} className="star" value={ratingValue} icon={faStar} size={'2x'} color={ratingValue <= this.props.rating ? '#ffc107' : '#e4e5e9'}/>
                 </button>
-                {/* </label> */}
               </>
             )
           })
