22e4b9c5dd9ac79f1c4fa494042c2a554c0e3fe6
Isabell Lee <izzy.isabell@gmail.com>
2021-09-18 20:22:33 -0700

    changed class to classname for soc btns

--- client/src/Components/ProductOverview/POComponents/SocialMediaButtons.jsx
+++ client/src/Components/ProductOverview/POComponents/SocialMediaButtons.jsx
@@ -4,8 +4,8 @@ var SocialMediaButtons = (props) => {
   return (
     <div className='po-soc-med-btns'>
       <i className='fab fa-facebook fa-2x soc-btn'></i>
-      <i class='fab fa-twitter fa-2x soc-btn'></i>
-      <i class='fab fa-pinterest fa-2x soc-btn'></i>
+      <i className='fab fa-twitter fa-2x soc-btn'></i>
+      <i className='fab fa-pinterest fa-2x soc-btn'></i>
     </div>
   );
 };
