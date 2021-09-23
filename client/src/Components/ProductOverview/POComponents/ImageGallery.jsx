a9e8a2d288c5c64822af479b4ad6350fdcca8a8e
Isabell Lee <izzy.isabell@gmail.com>
2021-09-23 13:04:08 -0700

    in progress with using placeholder image when null

--- client/src/Components/ProductOverview/POComponents/ImageGallery.jsx
+++ client/src/Components/ProductOverview/POComponents/ImageGallery.jsx
@@ -172,7 +172,7 @@ class ImageGallery extends React.Component {
                       key={index}
                       id={this.state.zoomed ? 'zoomed-slide' : 'carousel__slide'}
                       className={this.state.expanded ? 'carousel__slide expanded-slide' : 'carousel__slide'}
-                      src={image ? image : 'client/dist/images/default-placeholder.png'}
+                      src={image ? image : '/client/dist/images/default-placeholder.png'}
                       onClick={!this.state.expanded ? this.expandSlide : this.zoomSlide}
                       onMouseMove={this.state.zoomed ? this.handleZoomMouseMove : null}
                       style={{
