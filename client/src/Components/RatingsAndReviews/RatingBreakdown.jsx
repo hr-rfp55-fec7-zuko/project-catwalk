717db06d61402c5de9d3beb87198fbe87813f9b7
Caitlin Kinney <thecaitlinkinney@gmail.com>
2021-09-22 20:40:25 -0700

    Cleanup started

--- client/src/Components/RatingsAndReviews/RatingBreakdown.jsx
+++ client/src/Components/RatingsAndReviews/RatingBreakdown.jsx
@@ -36,6 +36,7 @@ class RatingBreakdown extends React.Component {
 
         <div className="rating-percentage-recommended">{percentageRecommended}% of reviews recommend this product</div>
         <br/>
+        
         {ratingsTuples.map((ratingTuple) =>
           <RatingBar key={ratingTuple[0]} reviewCount={reviewCount} ratingTuple={ratingTuple} toggleStarRatingFilter={this.props.toggleStarRatingFilter} handleClearStarFilters={this.handleClearStarFilters}/>)}
 
